a1fa27570311994231dd7ef47e4aebb568dc5a82
shalom-19 <113606094+shalom-19@users.noreply.github.com>
2022-11-22 19:19:10 +0100

    Shalom Commit

---
+++ print_decimal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_decimal - prints a digit on the stdout
+ * @value: parameter passed
+ * Return: count
+ */
+int print_decimal(int value)
+{
+	int count = 0;
+
+	if (value >= 0)
+	{
+
+	}
+	return (count);
+}
